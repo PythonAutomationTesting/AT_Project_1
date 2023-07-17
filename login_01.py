7fce5d19b11e75b4333c4ca18249e024967f5cb5
PythonAutomationTesting <125179887+PythonAutomationTesting@users.noreply.github.com>
2023-07-17 12:21:49 +0530

    Add files via upload

---
+++ login_01.py
@@ -0,0 +1,43 @@
+"""Locating Elements on the Web page"""
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from time import sleep
+
+# To use the Python Selenium Selector you have to use the By class
+from selenium.webdriver.common.by import By
+
+
+class Prathyusha:
+    '''To login the orange HRM portal'''
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    username = 'Admin'
+    password = 'admin123'
+    # username_locator is a TagName
+    username_locator = 'username'
+    # password_locator is a TagName
+    password_locator = 'password'
+    # Submit button Locator as XPATH
+    submitBox_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+
+    # Startup method for the CRM application
+    def Browsing(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)      
+
+
+    # Method for login into the CRM application
+    def login(self):
+        sleep(5)
+        self.driver.find_element(by=By.NAME, value=self.username_locator).send_keys(self.username)
+        self.driver.find_element(by=By.NAME, value=self.password_locator).send_keys(self.password)
+        self.driver.find_element(by=By.XPATH, value=self.submitBox_locator).click()
+
+        while(True):    # only for chrome and Edge browsers #
+            pass 
+
+
+Prathyusha().Browsing()
+Prathyusha().login()
